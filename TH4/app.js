8af1961f59107c364c9ccc0ddc03bee6469e6b5a
Anh Tuyet <anhtuyet3152005@gmail.com>
2026-03-27 15:54:23 +0700

    update TH4: sua blog

--- TH4/app.js
+++ TH4/app.js
@@ -10,22 +10,21 @@ app.set('view engine', 'ejs');
 
 // Kết nối MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/blogDB')
-  .then(() => console.log('Kết nối MongoDB thành công'))
-  .catch(err => console.log(err));
+.then(() => console.log('Kết nối MongoDB thành công'))
+.catch(err => console.log(err));
 
-
-// ===== TRANG CHỦ =====
+// Trang chủ (sắp xếp bài mới nhất lên trên)
 app.get('/', async (req, res) => {
   const posts = await BlogPost.find({}).sort({ _id: -1 });
   res.render('index', { posts });
 });
 
-
-// ===== TẠO =====
+// Form tạo
 app.get('/blogposts/new', (req, res) => {
   res.render('create');
 });
 
+// Lưu bài viết
 app.post('/blogposts/store', async (req, res) => {
   await BlogPost.create({
     title: req.body.title,
@@ -34,27 +33,21 @@ app.post('/blogposts/store', async (req, res) => {
   res.redirect('/');
 });
 
-
-// ===== CHI TIẾT =====
+// Xem chi tiết
 app.get('/blogposts/:id', async (req, res) => {
   const post = await BlogPost.findById(req.params.id);
   res.render('detail', { post });
 });
 
-
-// ===== XOÁ =====
-app.get('/delete/:id', async (req, res) => {
-  await BlogPost.findByIdAndDelete(req.params.id);
-  res.redirect('/');
-});
-
-
 // ===== SỬA =====
+
+// Hiển thị form sửa
 app.get('/edit/:id', async (req, res) => {
   const post = await BlogPost.findById(req.params.id);
   res.render('edit', { post });
 });
 
+// Cập nhật bài viết
 app.post('/update/:id', async (req, res) => {
   await BlogPost.findByIdAndUpdate(req.params.id, {
     title: req.body.title,
@@ -63,6 +56,11 @@ app.post('/update/:id', async (req, res) => {
   res.redirect('/');
 });
 
+// ===== XOÁ =====
+app.get('/delete/:id', async (req, res) => {
+  await BlogPost.findByIdAndDelete(req.params.id);
+  res.redirect('/');
+});
 
 app.listen(3000, () => {
   console.log('Server chạy tại http://localhost:3000');
